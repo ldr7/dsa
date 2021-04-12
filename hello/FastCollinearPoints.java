61a33ca7698ff20812efc5d0c8627b43f5d9089b
Atharva <atharvajoshi011@gmail.com>
2021-04-13 02:47:43 +0530

    Week 3 Part 3
    Signed-off-by: Atharva <atharvajoshi011@gmail.com>

--- hello/FastCollinearPoints.java
+++ hello/FastCollinearPoints.java
@@ -38,7 +38,87 @@ public class FastCollinearPoints {
 
     public LineSegment[] segments() {
         // the line segments
-        
+        LineSegment[] lineSegments = new LineSegment[pointsSet.length];
+        int collinearCount = 0;
+        for (int i = 0; i < pointsSet.length; i++) {
+            Point p = pointsSet[i];
+            double[] slopeToPoint = new double[pointsSet.length - 1];
+            double[] aux = new double[pointsSet.length - 1];
+            double[] copy = new double[pointsSet.length - 1];
+            for (int j = 0; j < pointsSet.length; j++) {
+                if (j != i) {
+                    double slope = p.slopeTo(pointsSet[j]);
+                    slopeToPoint[j] = slope;
+                    aux[j] = slope;
+                    copy[j] = slope;
+                }
+            }
+            mergeSort(slopeToPoint, aux, 0, pointsSet.length - 2);
+            int collinearIndex = calcSegment(slopeToPoint);
+            if (collinearIndex != -1)
+                lineSegments[collinearCount++] = new LineSegment(p, pointsSet[recoverOrgPoint(copy,
+                                                                                              slopeToPoint[collinearIndex])]);
+        }
+        count = collinearCount;
+        return lineSegments;
+    }
+
+    private int recoverOrgPoint(double[] slopeCopy, double slope) {
+        int index = -1;
+        for (int i = 0; i < slopeCopy.length; i++) {
+            if (slope == slopeCopy[i])
+                index = i;
+        }
+        return index;
+    }
+
+    private static void mergeSort(double[] slopeToPoint, double[] aux, int low, int high) {
+        if (high <= low)
+            return;
+        int mid = low + (high - low) / 2;
+        mergeSort(slopeToPoint, aux, low, mid);
+        mergeSort(slopeToPoint, aux, mid + 1, high);
+        if (slopeToPoint[mid + 1] > slopeToPoint[mid])
+            return;
+        merge(slopeToPoint, aux, low, mid, high);
+    }
+
+    private static void merge(double[] slopeToPoint, double[] aux, int low, int mid, int high) {
+        for (int k = 0; k < slopeToPoint.length; k++) {
+            aux[k] = slopeToPoint[k];
+        }
+        int i = low;
+        int j = mid + 1;
+        for (int k = 0; k <= high; k++) {
+            if (i > mid)
+                slopeToPoint[k] = slopeToPoint[j++];
+            else if (j > high)
+                slopeToPoint[k] = slopeToPoint[i++];
+            else if (aux[i] > aux[j])
+                slopeToPoint[k] = aux[j++];
+            else
+                slopeToPoint[k] = aux[i++];
+        }
+    }
+
+    private int calcSegment(double[] slopeToPoint) {
+        int collinearIndex = -1;
+        for (int i = 1; i < slopeToPoint.length; i++) {
+            if (slopeToPoint[i] == slopeToPoint[i - 1]) {
+                int start = i - 1;
+                int j = i;
+                int collinearcount = 2;
+                while (slopeToPoint[j + 1] == slopeToPoint[j]) {
+                    collinearcount += 1;
+                    j += 1;
+                }
+                if (collinearcount >= 3) {
+                    collinearIndex = start;
+                    break;
+                }
+            }
+        }
+        return collinearIndex;
     }
 
     public static void main(String[] args) {
